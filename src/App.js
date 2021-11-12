804a998ebaa116c5501f781a9bd069debb90b188
Mathias Desloges <mathias.desloges@bell.ca>
2021-11-12 16:21:50 -0500

    implement learning tools

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useRef } from "react";
-// import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 
 import Webcam from "react-webcam";
-import "./App.css";
 import { drawHand } from "./utilities";
 import { default as poseDetectorNet} from "./PoseDetectorNet";
 
@@ -12,47 +10,111 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  // MODE
+  const MODE_LOADING = 0
+  const MODE_IDLE = 1
+  const MODE_COLLECTING = 2
+  const MODE_TRAINNING = 3
+  const [mode, setMode] = useState(MODE_LOADING)
+  const [label, setRawLabel] = useState([0,0])
+
+  const [labelInput, setLabelInput] = useState(JSON.stringify(label))
+
+  const [dataCollector, setDataCollector] = useState([])
+ 
+  const net = useRef(null);
+  const poseDetector = useRef(null);
+
   useEffect(() => {
     (async () => {
-      const net = await handpose.load();
-      const poseDetector = poseDetectorNet.load();
-      const video = webcamRef.current.video;
-  
+      net.current = await handpose.load();
       console.log("Handpose model loaded.");
-      //  Loop and detect hands
-      setInterval(async () => {
-        if (
-          typeof webcamRef.current !== "undefined" &&
-          webcamRef.current !== null &&
-          webcamRef.current.video.readyState === 4
-        ) {
-          // Get Video Properties
-          const video = webcamRef.current.video;
-          const videoWidth = webcamRef.current.video.videoWidth;
-          const videoHeight = webcamRef.current.video.videoHeight;
-    
-          // Set video width
-          webcamRef.current.video.width = videoWidth;
-          webcamRef.current.video.height = videoHeight;
-    
-          // Set canvas height and width
-          canvasRef.current.width = videoWidth;
-          canvasRef.current.height = videoHeight;
-
-          // Make Detections
-          const hands = await net.estimateHands(video);
-          console.log(hands);
-
-          // hands[0].landmarks
-          // if () 
-
-          // Draw mesh
-          const ctx = canvasRef.current.getContext("2d");
-          drawHand(hands, ctx);
-        }        
-      }, 100);        
+      poseDetector.current = poseDetectorNet.load();
+      setMode(MODE_IDLE)
     })()
   }, [])
+
+  useEffect(() => {
+    //  Loop and detect hands
+    const int = setInterval(async () => {
+      if (
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4
+      ) {
+        // Get Video Properties
+        const video = webcamRef.current.video;
+        const videoWidth = webcamRef.current.video.videoWidth;
+        const videoHeight = webcamRef.current.video.videoHeight;
+  
+        // Set video width
+        webcamRef.current.video.width = videoWidth;
+        webcamRef.current.video.height = videoHeight;
+  
+        // Set canvas height and width
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+
+        // Make Detections
+        const hands = await net.current.estimateHands(video);
+
+        if (hands.length) {
+          if (mode === MODE_COLLECTING) {
+            setDataCollector ([...dataCollector, { inputs: hands[0].landmarks, labels: label}])
+          }
+          if (mode === MODE_IDLE) {
+            poseDetector.current.estimatePoses(rawHandsToDataset([{ inputs: hands[0].landmarks }]))
+          }
+        } 
+
+        // Draw mesh
+        const ctx = canvasRef.current.getContext("2d");
+        drawHand(hands, ctx);
+      }
+    }, 50);
+    return () => clearInterval(int)
+  }, [dataCollector, label, mode])
+
+  const toogleCollectState = () => {
+    if (mode === MODE_IDLE)
+      setMode(MODE_COLLECTING)
+    else if (mode === MODE_COLLECTING) {
+      setMode(MODE_IDLE)
+      console.log(dataCollector)
+      localStorage.setItem('trainningData', JSON.stringify(dataCollector))
+    }
+  }
+
+  const rawHandsToDataset = (data) => {
+    const labels = [], inputs = []
+    data.forEach(entry => { 
+      inputs.push(entry.inputs.map(i => [i[0] - entry.inputs[0][0], i[1] - entry.inputs[0][1], i[2] - entry.inputs[0][2]]).flat())
+      if (entry.labels)
+        labels.push(entry.labels)
+    })
+    return {
+      inputs,
+      labels
+    }
+  }
+
+  const train = async () => {
+    setMode(MODE_TRAINNING)
+    if (dataCollector.length) {
+      const data = rawHandsToDataset(dataCollector)
+      await poseDetector.current.train(data)
+    }
+    setMode(MODE_IDLE)
+  }
+
+  const setLabel = (str) => {
+    setRawLabel(JSON.parse(str))
+  }
+
+  const loadTrainningData = async () => {
+    const data = await import ('./trainningData.json')
+    setDataCollector(data.default)
+  }
 
   return (
     <div className="App">
@@ -62,6 +124,7 @@ function App() {
           position: "absolute",
           marginLeft: "auto",
           marginRight: "auto",
+          top: 150,
           left: 0,
           right: 0,
           textAlign: "center",
@@ -77,6 +140,7 @@ function App() {
           position: "absolute",
           marginLeft: "auto",
           marginRight: "auto",
+          top: 150,
           left: 0,
           right: 0,
           textAlign: "center",
@@ -85,8 +149,21 @@ function App() {
           height: 480,
         }}
       />
-
-
+      { mode !== MODE_LOADING && (
+        <fieldset>
+          <legend>Trainning</legend>
+          { mode === MODE_COLLECTING && (<span>collecting... {dataCollector.length ? (<React.Fragment>{dataCollector.length} frame(s)</React.Fragment>) : null} </span>)}
+          { mode === MODE_TRAINNING && (<span>trainning...</span>)}
+          <fieldset>
+            <legend>current label : {JSON.stringify(label)}</legend>
+            <label htmlFor="new_label">update to</label><input id="new_label" value={labelInput} onChange={e => setLabelInput(e.target.value)} />
+            <button onClick={ () => setLabel(labelInput) } >update !</button>
+          </fieldset>
+          <button onClick={ loadTrainningData } >Load trainning data !</button>
+          <button onClick={ toogleCollectState } disabled={[MODE_TRAINNING].includes(mode)} >collect !</button>
+          <button onClick={ train } disabled={[MODE_COLLECTING, MODE_TRAINNING].includes(mode) || !dataCollector.length} >train !</button>
+        </fieldset>
+      )}
     </div>
   );
 }
--- src/PoseDetectorNet/index.js
+++ src/PoseDetectorNet/index.js
@@ -7,9 +7,9 @@ function createModel() {
   // Add a single input layer
   model.add(tf.layers.dense({inputShape: [63], units: 50, activation: 'relu'}));
 
-  model.add(tf.layers.dense({inputShape: [1], units: 50, activation: 'relu'}));
-  model.add(tf.layers.dense({inputShape: [1], units: 50, activation: 'relu'}));
-  model.add(tf.layers.dense({inputShape: [1], units: 50, activation: 'relu'}));
+  model.add(tf.layers.dense({units: 50, activation: 'relu'}));
+  model.add(tf.layers.dense({units: 50, activation: 'relu'}));
+  model.add(tf.layers.dense({units: 50, activation: 'relu'}));
 
   // Add an output layer
   model.add(tf.layers.dense({units: 2, activation: 'softmax'}));
@@ -22,8 +22,8 @@ function convertToTensor(data, normalizationData) {
     if (!normalizationData)
       tf.util.shuffle(data)
 
-    const inputs = data.map(d => d.inputs)
-    const labels = data.map(d => d.labels);
+    const inputs = data.inputs
+    const labels = data.labels
 
     const inputTensor = tf.tensor2d(inputs, [inputs.length, 63]);
     const labelTensor = tf.tensor2d(labels, [labels.length, 2]);
@@ -72,11 +72,11 @@ export default {
     let normalizationData
 
     return {
-      setTrainningDataset (datasets) {
-        [trainningData, normalizationData] = convertToTensor(datasets);  
+      setTrainningDatasets (datasets) {
+        return [trainningData, normalizationData] = convertToTensor(datasets);
       },
     
-      async train(trainningParameter = { batchSize: 32, epochs: 50 }) {
+      async train(datasets, trainningParameter = { batchSize: 32, epochs: 50 }) {
         model.compile({
           optimizer: tf.train.adam(),
           loss: tf.losses.meanSquaredError,
@@ -85,7 +85,7 @@ export default {
       
         const {batchSize, epochs} = trainningParameter;
   
-        const { inputs, labels } = trainningData
+        const [{ inputs, labels}] = this.setTrainningDatasets(datasets)
     
         return await model.fit(inputs, labels, {
           batchSize,
@@ -95,20 +95,22 @@ export default {
       },
     
       estimatePoses(dataset) {
-        const {labelMin, labelMax} = normalizationData;
+        if (normalizationData) {
+          const {labelMin, labelMax} = normalizationData;
 
-        tf.tidy(() => {
-      
-          // let xs = tf.linspace(0, 1, 100);
-          const [{inputs}] = convertToTensor([dataset], normalizationData)
-          
-          const preds = model.predict(inputs);;
-          const unNormPreds = preds
-            .mul(labelMax.sub(labelMin))
-            .add(labelMin);
-
-          console.log(unNormPreds)
-        })
+          tf.tidy(() => {
+        
+            // let xs = tf.linspace(0, 1, 100);
+            const [{inputs}] = convertToTensor(dataset, normalizationData)
+            
+            const preds = model.predict(inputs);;
+            const unNormPreds = preds
+              .mul(labelMax.sub(labelMin))
+              .add(labelMin);
+  
+            unNormPreds.print()
+          })  
+        }
       }
     }
   }
