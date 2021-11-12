e554fb4ac969d3966c2089c7da1592285244d814
FreakDev <freakdev@gmail.com>
2021-11-12 12:20:14 -0500

    posenet first untested implementation

--- src/App.js
+++ src/App.js
@@ -1,98 +1,92 @@
-// 1. Install dependencies DONE
-// 2. Import dependencies DONE
-// 3. Setup webcam and canvas DONE
-// 4. Define references to those DONE
-// 5. Load handpose DONE
-// 6. Detect function DONE
-// 7. Drawing utilities DONE
-// 8. Draw functions DONE
-
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 // import logo from './logo.svg';
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
+
 import Webcam from "react-webcam";
 import "./App.css";
 import { drawHand } from "./utilities";
+import { default as poseDetectorNet} from "./PoseDetectorNet";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  const runHandpose = async () => {
-    const net = await handpose.load();
-    console.log("Handpose model loaded.");
-    //  Loop and detect hands
-    setInterval(() => {
-      detect(net);
-    }, 100);
-  };
-
-  const detect = async (net) => {
-    // Check data is available
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      // Get Video Properties
+  useEffect(() => {
+    (async () => {
+      const net = await handpose.load();
+      const poseDetector = poseDetectorNet.load();
       const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+  
+      console.log("Handpose model loaded.");
+      //  Loop and detect hands
+      setInterval(async () => {
+        if (
+          typeof webcamRef.current !== "undefined" &&
+          webcamRef.current !== null &&
+          webcamRef.current.video.readyState === 4
+        ) {
+          // Get Video Properties
+          const video = webcamRef.current.video;
+          const videoWidth = webcamRef.current.video.videoWidth;
+          const videoHeight = webcamRef.current.video.videoHeight;
+    
+          // Set video width
+          webcamRef.current.video.width = videoWidth;
+          webcamRef.current.video.height = videoHeight;
+    
+          // Set canvas height and width
+          canvasRef.current.width = videoWidth;
+          canvasRef.current.height = videoHeight;
 
-      // Set video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+          // Make Detections
+          const hands = await net.estimateHands(video);
+          console.log(hands);
 
-      // Set canvas height and width
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
+          // hands[0].landmarks
+          // if () 
 
-      // Make Detections
-      const hand = await net.estimateHands(video);
-      console.log(hand);
-
-      // Draw mesh
-      const ctx = canvasRef.current.getContext("2d");
-      drawHand(hand, ctx);
-    }
-  };
-
-  runHandpose();
+          // Draw mesh
+          const ctx = canvasRef.current.getContext("2d");
+          drawHand(hands, ctx);
+        }        
+      }, 100);        
+    })()
+  }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
-      </header>
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+
+
     </div>
   );
 }
---
+++ src/PoseDetectorNet/index.js
@@ -0,0 +1,115 @@
+import * as tf from "@tensorflow/tfjs"
+
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential();
+
+  // Add a single input layer
+  model.add(tf.layers.dense({inputShape: [63], units: 50, activation: 'relu'}));
+
+  model.add(tf.layers.dense({inputShape: [1], units: 50, activation: 'relu'}));
+  model.add(tf.layers.dense({inputShape: [1], units: 50, activation: 'relu'}));
+  model.add(tf.layers.dense({inputShape: [1], units: 50, activation: 'relu'}));
+
+  // Add an output layer
+  model.add(tf.layers.dense({units: 2, activation: 'softmax'}));
+
+  return model;
+}
+
+function convertToTensor(data, normalizationData) {
+  return tf.tidy(() => {
+    if (!normalizationData)
+      tf.util.shuffle(data)
+
+    const inputs = data.map(d => d.inputs)
+    const labels = data.map(d => d.labels);
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 63]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 2]);
+
+    //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    
+    let inputMax;
+    let inputMin;
+    let labelMax;
+    let labelMin;
+
+    if (normalizationData) {
+      inputMax = normalizationData.inputMax;
+      inputMin = normalizationData.inputMin;
+      labelMax = normalizationData.labelMax;
+      labelMin = normalizationData.labelMin;  
+    } else {
+      inputMax = inputTensor.max();
+      inputMin = inputTensor.min();
+      labelMax = labelTensor.max();
+      labelMin = labelTensor.min();  
+    }
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+    // debugger
+
+    return [{
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+    }, {
+      // Return the min/max bounds so we can use them later.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    }]
+  });
+}
+
+export default {
+  load() {
+    const model = createModel()
+    let trainningData
+    let normalizationData
+
+    return {
+      setTrainningDataset (datasets) {
+        [trainningData, normalizationData] = convertToTensor(datasets);  
+      },
+    
+      async train(trainningParameter = { batchSize: 32, epochs: 50 }) {
+        model.compile({
+          optimizer: tf.train.adam(),
+          loss: tf.losses.meanSquaredError,
+          metrics: ['mse'],
+        });
+      
+        const {batchSize, epochs} = trainningParameter;
+  
+        const { inputs, labels } = trainningData
+    
+        return await model.fit(inputs, labels, {
+          batchSize,
+          epochs,
+          shuffle: true
+        });
+      },
+    
+      estimatePoses(dataset) {
+        const {labelMin, labelMax} = normalizationData;
+
+        tf.tidy(() => {
+      
+          // let xs = tf.linspace(0, 1, 100);
+          const [{inputs}] = convertToTensor([dataset], normalizationData)
+          
+          const preds = model.predict(inputs);;
+          const unNormPreds = preds
+            .mul(labelMax.sub(labelMin))
+            .add(labelMin);
+
+          console.log(unNormPreds)
+        })
+      }
+    }
+  }
+}
